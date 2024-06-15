4c35fff2a7db60ea21d43ea2d66d8bbb4886504b
Nikita Kisiura <nikitar156@gmail.com>
2024-06-15 17:12:21 -0400

    fixed api-constants

--- src/app/shared/constants/api.constants.ts
+++ src/app/shared/constants/api.constants.ts
@@ -1,5 +1,5 @@
 export const API_BASE_URL = "https://rickandmortyapi.com/api";
 
 export const CHARACTERS_URL = "character";
-export const LOCATIONS_URL = "locations";
-export const EPISODES_URL = "episodes";
+export const LOCATIONS_URL = "location";
+export const EPISODES_URL = "episode";
