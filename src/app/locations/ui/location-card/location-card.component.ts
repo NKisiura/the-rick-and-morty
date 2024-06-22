cb488d1c3146aefc9531b34ba6c3aafe06535581
Nikita Kisiura <nikitar156@gmail.com>
2024-06-21 22:59:06 -0400

    created location-card component

---
+++ src/app/locations/ui/location-card/location-card.component.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component, input } from "@angular/core";
+import { Location } from "@locations/types";
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: "app-location-card",
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: "./location-card.component.html",
+  styleUrl: "./location-card.component.scss",
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LocationCardComponent {
+  public location = input.required<Location>();
+}
