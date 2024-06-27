a35f20202ed6b6e76197b061b6c2f724217de5f9
Nikita Kisiura <nikitar156@gmail.com>
2024-06-26 20:16:26 -0400

    app header component in-progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, ChangeDetectionStrategy } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { HeaderComponent } from "@core/ui";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush,
