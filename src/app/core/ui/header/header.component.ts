f51fa1ce5d14e024a8604a73739932c1b9a4dfa3
Nikita Kisiura <nikitar156@gmail.com>
2024-07-02 18:43:36 -0400

    created footer component

--- src/app/core/ui/header/header.component.ts
+++ src/app/core/ui/header/header.component.ts
@@ -17,6 +17,11 @@ import { NgOptimizedImage } from "@angular/common";
 })
 export class HeaderComponent {
   public APP_ROUTES = APP_ROUTES;
+  public headerLinks: APP_ROUTES[] = [
+    this.APP_ROUTES.CHARACTERS,
+    this.APP_ROUTES.EPISODES,
+    this.APP_ROUTES.LOCATIONS,
+  ];
   public routerLinkActiveOptions: IsActiveMatchOptions = {
     matrixParams: "ignored",
     queryParams: "ignored",
