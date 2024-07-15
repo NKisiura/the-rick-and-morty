6ba5b2b795f9d15a1ee5b8f111a2ce934742d269
Nikita Kisiura <nikitar156@gmail.com>
2024-07-15 18:47:18 -0400

    page not found component styled

--- src/app/core/ui/page-not-found/page-not-found.component.ts
+++ src/app/core/ui/page-not-found/page-not-found.component.ts
@@ -1,11 +1,20 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { NgOptimizedImage } from "@angular/common";
+import { FaIconComponent } from "@fortawesome/angular-fontawesome";
+import { faArrowLeftLong } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: "app-page-not-found",
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage, FaIconComponent],
   templateUrl: "./page-not-found.component.html",
   styleUrl: "./page-not-found.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PageNotFoundComponent {}
+export class PageNotFoundComponent {
+  public goBackIcon = faArrowLeftLong;
+
+  public goBack(): void {
+    history.back();
+  }
+}
