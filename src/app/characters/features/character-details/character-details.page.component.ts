3e038963ccdd41d46fe8e01d9ea1e545bb3cbeac
Nikita Kisiura <nikitar156@gmail.com>
2024-07-22 18:01:09 -0400

    character-details page in-progress

--- src/app/characters/features/character-details/character-details.page.component.ts
+++ src/app/characters/features/character-details/character-details.page.component.ts
@@ -9,12 +9,22 @@ import { NgOptimizedImage } from "@angular/common";
 import { RouterLink } from "@angular/router";
 import { APP_ROUTES } from "@shared/constants";
 import { CharacterDetailsStore } from "./character-details.store";
+import { FaIconComponent } from "@fortawesome/angular-fontawesome";
+import { faArrowLeftLong } from "@fortawesome/free-solid-svg-icons";
+import { ErrorMessageComponent, LoaderComponent } from "@shared/components";
 import { CharacterPropertyWrapperComponent } from "@characters/ui";
 
 @Component({
   selector: "app-character-details",
   standalone: true,
-  imports: [NgOptimizedImage, RouterLink, CharacterPropertyWrapperComponent],
+  imports: [
+    NgOptimizedImage,
+    RouterLink,
+    FaIconComponent,
+    LoaderComponent,
+    ErrorMessageComponent,
+    CharacterPropertyWrapperComponent,
+  ],
   providers: [CharacterDetailsStore],
   templateUrl: "./character-details.page.component.html",
   styleUrl: "./character-details.page.component.scss",
@@ -22,7 +32,9 @@ import { CharacterPropertyWrapperComponent } from "@characters/ui";
 })
 export class CharacterDetailsPageComponent implements OnInit {
   private readonly characterDetailsStore = inject(CharacterDetailsStore);
+
   public APP_ROUTES = APP_ROUTES;
+  public backToCharactersIcon = faArrowLeftLong;
 
   public characterId = input.required<number, string | number>({
     transform: (value: string | number) => +value,
@@ -40,5 +52,9 @@ export class CharacterDetailsPageComponent implements OnInit {
     this.characterDetailsStore.characterWithEpisodesByIdRequested(
       this.characterId(),
     );
+  }
+
+  public goBackToCharactersPage(): void {
+    history.back();
   }
 }
